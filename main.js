79ac9656cbb7355f3f9f0c9afeb0c1d4e44df2a7
Frrank Sarabia <frrankemergk@hotmail.com>
2024-07-12 22:54:04 -0500

    actualizacion IMC

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-// let contenedor= document.getElementById("caja")
-// console.log(contenedor);
 
-// contenedor.addEventListener('click', (o) => {
-//     console.log(o.target.classList);
-//     if (o.target.classList.contains('cuadrado')) {
-//         o.target.classList.toggle('circulo')
-
-//     }
-
-// })
 
 let botonCalcularMasa = document.getElementById("botonCalcularMasa")
 
-botonCalcularMasa.addEventListener('click', (e) => {
+let imputEstatura = document.getElementById("estatura")
+let imputPeso = document.getElementById("peso")
 
-    let imputEstatura = document.getElementById("estatura")
-    let imputPeso = document.getElementById("peso")
+
+imputEstatura.addEventListener('keyup', (e) => {
+    if (parseInt(e.target.value) <= 0) {
+        imputEstatura.value = "";
+    }
+})
+
+imputPeso.addEventListener('keydown', (e) => {
+    if (parseInt(e.target.value) <= 0) {
+        imputPeso.value = "";
+    }
+})
+
+
+
+botonCalcularMasa.addEventListener('click', (e) => {
 
     let valorPeso = imputPeso.value
     let ValorEstatura = imputEstatura.value / 100
 
-
     let resultadoMasa = ((valorPeso) / (Math.pow(ValorEstatura, 2)))
-    console.log(resultadoMasa);
 
     let imputResultado = document.getElementById("resultado")
     imputResultado.value = resultadoMasa.toFixed(1)
-    
+
 })
 
