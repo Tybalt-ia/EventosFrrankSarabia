f4062d29a3335b6a5b86e329ce032cabe4d79265
Frrank Sarabia <frrankemergk@hotmail.com>
2024-07-16 18:03:49 -0500

    actualizacion notas

--- notas.js
+++ notas.js
@@ -1,4 +1,4 @@
-Notas: [
+let Notas= [
     {
         id: 0,
         titulo: 'sacara la basura',
@@ -27,11 +27,40 @@ Notas: [
 
 let idGlobal = 3
 function pintarNotas(Notas) {
+    console.log("algo");
+    
     for (let i = 0; i < Notas.length; i++) {
 
         let contenedor = document.getElementById("contenedorNotas")
 
         let tarjeta = document.createElement('div')
-       
+        tarjeta.className = "card m-1 text-center mb-3"
+        tarjeta.innerHTML = `
+              <div class="card-body">
+    <h5 class="card-title">${Notas[i].titulo} </h5>
+    <p class="card-text">${Notas[i].texto}</p>
+    <a href="#" class="btn btn-primary"> Borrar nota </a>
+  </div>    
+        `
+        contenedor.appendChild(tarjeta)
     }
+    
 }
+pintarNotas(Notas)
+
+let borrar = document.getElementById ("botonBorrar")
+botonBorrar.addEventListener('click', (e) => {
+
+    let Titulo = document.getElementById ("Titulo")
+    let Nota = document.getElementById ("Nota")
+    
+    titulo.addEventListener('keyup', (e) => {
+        if (parseInt(e.target.value) <= 0) {
+            imputEstatura.value = "";
+        }
+    })
+    
+
+
+   
+})
