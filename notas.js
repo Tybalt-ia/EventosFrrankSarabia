d8bec8eb21792c5faa0ed09e923c841bdb161533
Frrank Sarabia <frrankemergk@hotmail.com>
2024-07-16 16:24:17 -0500

    actualizacion notas

---
+++ notas.js
@@ -0,0 +1,37 @@
+Notas: [
+    {
+        id: 0,
+        titulo: 'sacara la basura',
+        texto: 'mi mama me va a retar si no lo hago',
+        realizada: false
+    },
+    {
+        id: 1,
+        titulo: 'lavar loza',
+        texto: 'se acomularia el desorden',
+        realizada: false
+    },
+    {
+        id: 2,
+        titulo: 'llamar a mama ',
+        texto: 'mi mama me va a enojar si no lo hago',
+        realizada: false
+    },
+    {
+        id: 3,
+        titulo: 'visitar a mary',
+        texto: 'se a sentir si no lo hago',
+        realizada: false
+    },
+]
+
+let idGlobal = 3
+function pintarNotas(Notas) {
+    for (let i = 0; i < Notas.length; i++) {
+
+        let contenedor = document.getElementById("contenedorNotas")
+
+        let tarjeta = document.createElement('div')
+       
+    }
+}
